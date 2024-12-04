3d9c78de23582276b09052531e4dc041ce7e9a75
Myhoo <47946007+MyhooSalsa@users.noreply.github.com>
2024-12-04 15:12:25 +0800

    Update tactile_sensor.py
    It seems pybullet has a conflict with tacto on headless server.  I have met a case that I can not get rgb&depth figures of statics and gripper correctly(they are all blank figures and their matrix is a zero-matrix) when I run this env on headless server, but the tactile figures are correct. My renderer works on egl which passes egl_check, and it can render other pybullet demos. Accidentally, I obtain the correct figure when I delete TactileSensor. After some imformation gathering, I have noticed some people said tacto pkg is based on opengl, I think maybe we can not use egl to render camera and use opengl to render tactile sensor at the same time.

--- calvin_env/camera/tactile_sensor.py
+++ calvin_env/camera/tactile_sensor.py
@@ -20,22 +20,28 @@ class TactileSensor(Camera):
         Returns:
             None
         """
-        self.cid = cid
-        self.name = name
-        self.robot_uid = robot_id
-        self.digits = tacto.Sensor(
-            width=width, height=height, visualize_gui=visualize_gui, config_path=os.path.join(REPO_BASE, config_path)
-        )
-        self.digits.add_camera(robot_id, digit_link_ids)  # env.robot.digit_links()
-        for obj in objects:
-            # self.digits.add_body(obj)
-            self.digits.add_object(obj.file.as_posix(), obj.uid, obj.global_scaling)
         self.visualize_gui = visualize_gui
+        if visualize_gui:
+        	self.cid = cid
+        	self.name = name
+        	self.robot_uid = robot_id
+        	self.digits = tacto.Sensor(
+            	width=width, height=height, visualize_gui=visualize_gui, config_path=os.path.join(REPO_BASE, config_path)
+        	)
+        	self.digits.add_camera(robot_id, digit_link_ids)  # env.robot.digit_links()
+        	for obj in objects:
+            	# self.digits.add_body(obj)
+            	self.digits.add_object(obj.file.as_posix(), obj.uid, obj.global_scaling)	
+        
 
     def render(self):
-        rgb, depth = self.digits.render()
-        if self.visualize_gui:
-            self.digits.updateGUI(rgb, depth)
-        rgb = np.concatenate(rgb, axis=2)
-        depth = np.stack(depth, axis=2)
+		if self.visualize_gui:
+        	rgb, depth = self.digits.render()
+        	if self.visualize_gui:
+            	self.digits.updateGUI(rgb, depth)
+        	rgb = np.concatenate(rgb, axis=2)
+        	depth = np.stack(depth, axis=2)
+		else:
+			rgb = np.zeros((160, 120, 6))
+        	depth = np.zeros((160, 120, 6))
         return rgb, depth
